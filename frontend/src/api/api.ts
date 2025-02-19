ad62796406bfe9d547c698504598158409a51250
Tayyabu <tb3286427@gmail.com>
2025-02-19 09:21:50 +0500

    add port number variable in frontend .env

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
-
-
-export const api = axios.create({baseURL:"http://localhost:3000",withCredentials:true}) 
+export const api = axios.create({
+  baseURL: `http://localhost:${import.meta.env.VITE_BACKEND_PORT || 3000}`,
+  withCredentials: true,
+});
