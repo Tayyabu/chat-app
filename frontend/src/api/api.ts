bf3935d980ba6f07e601f5a36d5ca64d4a3b43c0
Tayyabu <tb3286427@gmail.com>
2025-02-19 09:32:42 +0500

    redefine vite env variables

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: `http://localhost:${import.meta.env.VITE_BACKEND_PORT || 3000}`,
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
